96313e000fa1a14a24bc1fcd077565ca9293d1dc
Stanko Alexander <rakoth3d@gmail.com>
2009-11-28 04:06:31 +0300

    add smtp-tls plugin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,8 @@ config.action_view.cache_template_loading            = true
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
+require 'smtp-tls'
+
 config.action_mailer.raise_delivery_errors = true
 
 config.action_mailer.perform_deliveries = true
@@ -39,7 +41,6 @@ config.action_mailer.default_charset = "utf-8"
 #}
 
 config.action_mailer.smtp_settings = {
-	:enable_starttls_auto => true,
 	:address => 'smtp.gmail.com',
 	:port => 587,
   :user_name => "noreply.ellephoto@gmail.com",
